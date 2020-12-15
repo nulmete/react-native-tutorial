6b98323faa2cd9ac101143ef4f3df9f3af96dc2b
nicoulmete1 <nicoulmete1@gmail.com>
2020-12-15 11:44:15 -0300

    list & scrollview

--- App.js
+++ App.js
@@ -1,30 +1,34 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Button,
+  TextInput,
+} from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("nicolas");
-  const [age, setAge] = useState("40");
+  const [people, setPeople] = useState([
+    { name: "shaun", key: "1" },
+    { name: "yoshi", key: "2" },
+    { name: "mario", key: "3" },
+    { name: "luigi", key: "4" },
+    { name: "nicolas", key: "5" },
+    { name: "todd", key: "6" },
+    { name: "asdqwe", key: "7" },
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text>Enter name:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="e.g. John Doe"
-        onChangeText={(val) => setName(val)}
-      ></TextInput>
-      <Text>Enter age:</Text>
-      <TextInput
-        style={styles.input}
-        keyboardType="numeric"
-        placeholder="e.g. 25"
-        onChangeText={(val) => setAge(val)}
-      ></TextInput>
-      <Text>
-        Name: {name} - age: {age}
-      </Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        {people.map((p) => (
+          <Text style={styles.item} key={p.id}>
+            {p.name}
+          </Text>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -33,14 +37,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
-  input: {
-    borderWidth: 1,
-    borderColor: "#777",
-    padding: 8,
-    margin: 10,
-    width: 200,
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
   },
 });
